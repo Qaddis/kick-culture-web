f1aa670345d46d75f47b1fe3bec6b2fc0afa5fd0
Qaddis <svyatoslavb1107@gmail.com>
2024-04-01 20:38:34 +0300

    Added and styled Footer

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,16 +1,42 @@
+import { NavLink } from "react-router-dom";
+
 const Footer = () => {
 	return (
-		<footer>
-			<ul className="socials"></ul>
-			<nav className="footer-nav">
-				<ul>
-					<li>Home</li>
-					<li>Products</li>
-					<li>About</li>
-					<li>Basket</li>
+		<footer className="footer">
+			<div className="wrapper">
+				<ul className="socials">
+					<li>
+						<a href="https://github.com/qaddis" target="_blank">
+							<svg>
+								<use xlinkHref="#github-svg"></use>
+							</svg>
+						</a>
+					</li>
+					<li>
+						<a href="https://t.me/qaddis" target="_blank">
+							<svg>
+								<use xlinkHref="#telegram-svg"></use>
+							</svg>
+						</a>
+					</li>
+					<li>
+						<a href="https://vk.com/qaddis" target="_blank">
+							<svg>
+								<use xlinkHref="#vk-svg"></use>
+							</svg>
+						</a>
+					</li>
 				</ul>
-			</nav>
-			<p className="copyright"></p>
+				<p className="copyright">
+					© 2024 Святослав &quot;Qaddis&quot; Барсуков
+				</p>
+				<nav className="footer-nav">
+					<NavLink to="/">Home</NavLink>
+					<NavLink to="/products">Products</NavLink>
+					<NavLink to="/about">About</NavLink>
+					<NavLink to="/basket">Basket</NavLink>
+				</nav>
+			</div>
 		</footer>
 	);
 };
