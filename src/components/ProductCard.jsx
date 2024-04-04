f3983faa13cf144ad7ee01cedaf2a6160192bc30
Qaddis <svyatoslavb1107@gmail.com>
2024-04-04 16:29:30 +0300

    Styling of product cards has been corrected, display of the entire list of products has been added

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,16 +1,23 @@
 import styles from "./ProductCard.module.css";
+import PropTypes from "prop-types";
 
-const ProductCard = () => {
+const ProductCard = ({ title, price, image }) => {
 	return (
 		<article className={styles.card}>
-			<img src="/li_sneakers.png" className={styles.card__img} />
-			<h3 className={styles.card__header}>YoStep air 3</h3>
+			<img src={image} className={styles.card__img} />
+			<h3 className={styles.card__header}>{title}</h3>
 			<p className={styles.card__price}>
-				8990 <span className={styles.card__currency}>руб.</span>
+				{price} <span className={styles.card__currency}>usd</span>
 			</p>
 			<button className={styles.card__button}>To cart</button>
 		</article>
 	);
+};
+
+ProductCard.propTypes = {
+	title: PropTypes.string.isRequired,
+	price: PropTypes.number.isRequired,
+	image: PropTypes.string.isRequired,
 };
 
 export default ProductCard;
--- src/data.js
+++ src/data.js
@@ -2,41 +2,41 @@ export const cards = [
 	{
 		image: "/card_1.png",
 		title: "yostep air 3",
-		price: "",
+		price: 79.99,
 	},
 	{
 		image: "/card_2.png",
 		title: "yostep contrastmoon",
-		price: "",
+		price: 82.5,
 	},
 	{
 		image: "/card_3.png",
 		title: "yostep ego",
-		price: "",
+		price: 89.95,
 	},
 	{
 		image: "/card_4.png",
 		title: "yostep typhoon",
-		price: "",
+		price: 85.0,
 	},
 	{
 		image: "/card_5.png",
 		title: "yostep ego 2",
-		price: "",
+		price: 92.0,
 	},
 	{
 		image: "/card_6.png",
-		title: "yostep sunset",
-		price: "",
+		title: "yostep sunstroke",
+		price: 88.0,
 	},
 	{
 		image: "/card_7.png",
 		title: "yostep air 2",
-		price: "",
+		price: 78.5,
 	},
 	{
 		image: "/card_8.png",
 		title: "yostep air",
-		price: "",
+		price: 75.0,
 	},
 ];
