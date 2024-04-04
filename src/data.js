7765e8609021dec37848820f231d80a8290c3100
Qaddis <svyatoslavb1107@gmail.com>
2024-04-04 19:16:32 +0300

    Added display of popular products

--- src/data.js
+++ src/data.js
@@ -3,40 +3,48 @@ export const cards = [
 		image: "/card_1.png",
 		title: "yostep air 3",
 		price: 79.99,
+		isPopular: false,
 	},
 	{
 		image: "/card_2.png",
 		title: "yostep contrastmoon",
 		price: 82.5,
+		isPopular: false,
 	},
 	{
 		image: "/card_3.png",
 		title: "yostep ego",
 		price: 89.95,
+		isPopular: false,
 	},
 	{
 		image: "/card_4.png",
 		title: "yostep typhoon",
 		price: 85.0,
+		isPopular: true,
 	},
 	{
 		image: "/card_5.png",
 		title: "yostep ego 2",
 		price: 92.0,
+		isPopular: true,
 	},
 	{
 		image: "/card_6.png",
 		title: "yostep sunstroke",
 		price: 88.0,
+		isPopular: true,
 	},
 	{
 		image: "/card_7.png",
 		title: "yostep air 2",
 		price: 78.5,
+		isPopular: false,
 	},
 	{
 		image: "/card_8.png",
 		title: "yostep air",
 		price: 75.0,
+		isPopular: false,
 	},
 ];
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,13 +1,38 @@
 import styles from "./ProductsPage.module.css";
+import { useEffect, useState } from "react";
 
 import ProductCard from "../components/ProductCard";
 import { cards } from "../data";
 
 const Products = () => {
+	const [popularProducts, setPopularProducts] = useState([]);
+
+	const selectedProducts = () => {
+		let products = [];
+		for (let i = 0; i < cards.length; i++) {
+			if (cards[i].isPopular) products.push(cards[i]);
+		}
+
+		return products;
+	};
+
+	useEffect(() => {
+		setPopularProducts(selectedProducts);
+	}, []);
+
 	return (
 		<div className={styles.wrapper}>
 			<h2 className={styles.h2}>The best</h2>
-			<section className={styles.best}></section>
+			<section className={styles.best}>
+				{popularProducts.map((product) => (
+					<ProductCard
+						key={product.title}
+						title={product.title}
+						price={product.price}
+						image={product.image}
+					/>
+				))}
+			</section>
 			<h2 className={styles.h2}>All products</h2>
 			<section className={styles.products}>
 				{cards.map((card) => (
