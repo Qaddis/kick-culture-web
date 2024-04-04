d14631735a07e057505aa45470df62976da26874
Qaddis <svyatoslavb1107@gmail.com>
2024-04-04 14:40:36 +0300

    Added materials for product cards

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const cards = [
+	{
+		image: "/card_1.png",
+		title: "yostep air 3",
+		price: "",
+	},
+	{
+		image: "/card_2.png",
+		title: "yostep contrastmoon",
+		price: "",
+	},
+	{
+		image: "/card_3.png",
+		title: "yostep ego",
+		price: "",
+	},
+	{
+		image: "/card_4.png",
+		title: "yostep typhoon",
+		price: "",
+	},
+	{
+		image: "/card_5.png",
+		title: "yostep ego 2",
+		price: "",
+	},
+	{
+		image: "/card_6.png",
+		title: "yostep sunset",
+		price: "",
+	},
+	{
+		image: "/card_7.png",
+		title: "yostep air 2",
+		price: "",
+	},
+	{
+		image: "/card_8.png",
+		title: "yostep air",
+		price: "",
+	},
+];
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,16 +1,24 @@
 import styles from "./ProductsPage.module.css";
 
 import ProductCard from "../components/ProductCard";
+import { cards } from "../data";
 
 const Products = () => {
 	return (
 		<div className={styles.wrapper}>
 			<h2 className={styles.h2}>The best</h2>
-			<section className={styles.best}>
-				<ProductCard />
-			</section>
+			<section className={styles.best}></section>
 			<h2 className={styles.h2}>All products</h2>
-			<section className={styles.products}></section>
+			<section className={styles.products}>
+				{cards.map((card) => (
+					<ProductCard
+						key={card.title}
+						title={card.title}
+						image={card.image}
+						price={card.price}
+					/>
+				))}
+			</section>
 		</div>
 	);
 };
