91d0b6064082b7522a75a8d024a16103b7eaa39e
Qaddis <svyatoslavb1107@gmail.com>
2024-04-11 10:48:05 +0300

    About page added

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,14 +1,35 @@
 import { motion } from "framer-motion";
 
 const About = () => {
-	<motion.section
-		initial={{ y: "5%", opacity: 0 }}
-		animate={{ y: 0, opacity: 1 }}
-		exit={{ y: "-5%", opacity: 0, transition: { duration: 0.35 } }}
-		transition={{ delay: 0.35 }}
-	>
-		<h2>About Page</h2>
-	</motion.section>;
+	return (
+		<motion.section
+			initial={{ y: "5%", opacity: 0 }}
+			animate={{ y: 0, opacity: 1 }}
+			exit={{ y: "-5%", opacity: 0, transition: { duration: 0.35 } }}
+			transition={{ delay: 0.35 }}
+			className="about wrapper"
+		>
+			<h2 className="about__heading">About Page</h2>
+			<p>
+				<b className="name">Kick Culture</b> is not a real store. This is a pet
+				project made by{" "}
+				<a href="https://github.com/qaddis" target="_blank">
+					Qaddis
+				</a>
+				.
+			</p>
+			<p>
+				In it I tried to implement an example of a real store using{" "}
+				<b style={{ color: "#BD34FE" }}>Vite</b> +{" "}
+				<b style={{ color: "#149ECA" }}>React</b>,{" "}
+				<b style={{ color: "#149ECA" }}>React Router</b>,{" "}
+				<b style={{ color: "#F94DA3" }}>Framer Motion</b> and
+				<b style={{ color: "#BF4080" }}> SCSS</b>. Product cards were generated
+				by <b style={{ color: "#8966F8" }}>Leonardo AI</b>, the names were
+				invented by myself, any resemblance to real brands is accidental.
+			</p>
+		</motion.section>
+	);
 };
 
 export default About;
