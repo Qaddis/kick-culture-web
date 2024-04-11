74b05c6dcac8790ec405b4c9852887718e281608
Qaddis <svyatoslavb1107@gmail.com>
2024-04-11 11:29:04 +0300

    Not Found Page added

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,14 +1,18 @@
+import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
 const NotFound = () => {
 	return (
 		<motion.section
+			className="not-found wrapper"
 			initial={{ y: "5%", opacity: 0 }}
 			animate={{ y: 0, opacity: 1 }}
 			exit={{ y: "-5%", opacity: 0, transition: { duration: 0.35 } }}
 			transition={{ delay: 0.35 }}
 		>
-			<h2>Ooopsie</h2>
+			<h2>Ooopsie... 404</h2>
+			<h3>Page not found 🤔</h3>
+			<Link to="/">Oh s**t, go back!</Link>
 		</motion.section>
 	);
 };
