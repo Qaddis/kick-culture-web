d043b19034842e3b3589f1be56e7f8c83c46f55c
Qaddis <svyatoslavb1107@gmail.com>
2024-04-14 13:47:51 +0300

    Added more information on the About page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -19,14 +19,31 @@ const About = () => {
 				.
 			</p>
 			<p>
-				In it I tried to implement an example of a real store using{" "}
-				<b style={{ color: "#BD34FE" }}>Vite</b> +{" "}
-				<b style={{ color: "#149ECA" }}>React</b>,{" "}
-				<b style={{ color: "#149ECA" }}>React Router</b>,{" "}
-				<b style={{ color: "#F94DA3" }}>Framer Motion</b> and
+				In it I tried to implement an example of a real store using
+				<b style={{ color: "#BD34FE" }}> Vite</b> +
+				<b style={{ color: "#149ECA" }}> React</b>,
+				<b style={{ color: "#593D88" }}> Redux Toolkit</b>,
+				<b style={{ color: "#149ECA" }}> React Router</b>,
+				<b style={{ color: "#F94DA3" }}> Framer Motion</b> and
 				<b style={{ color: "#BF4080" }}> SCSS</b>. Product cards were generated
 				by <b style={{ color: "#8966F8" }}>Leonardo AI</b>, the names were
 				invented by myself, any resemblance to real brands is accidental.
+			</p>
+			<p>
+				Fonts used: <b style={{ fontFamily: "Peralta, sans" }}>Peralta</b>,
+				<b style={{ fontFamily: "Quicksand, sans-serif" }}> Quicksand</b>,
+				<b> Work sans</b>.
+			</p>
+			<p>
+				Icons taken from{" "}
+				<a
+					style={{ textShadow: "none", color: "var(--dark)" }}
+					href="https://www.iconfinder.com/"
+					target="_blank"
+				>
+					Iconfinder
+				</a>{" "}
+				(cart, left arrow, github, vk, telegram).
 			</p>
 		</motion.section>
 	);
