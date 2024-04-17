28366eadaf3e307acc28bd554b46c5c9040b164a
Qaddis <svyatoslavb1107@gmail.com>
2024-04-17 17:50:47 +0300

    The styles have been slightly changed, a creeping line has been added to the home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,33 +9,31 @@ const Home = () => {
 			exit={{ opacity: 0, transition: { duration: 0.35 } }}
 			transition={{ delay: 0.35 }}
 		>
-			<div className="landing">
+			<section className="landing">
 				<div className="wrapper">
-					<div className="short_info">
-						<h2 className="landing__h1">Kick Culture</h2>
-						<p className="landing__p">
-							Walk in
-							<i style={{ fontStyle: "normal", color: "#F06D58" }}> style</i>,
-							breathe in
-							<i style={{ fontStyle: "normal", color: "#FCED76" }}> culture</i>:
-							<br />
-							<i
-								style={{
-									fontStyle: "normal",
-									color: "var(--light)",
-									textShadow: "0 0 5px var(--violet)",
-								}}
-							>
-								Kick Culture
-							</i>{" "}
-							- in every pair, there&apos;s a history!
-						</p>
-						<Link to="/products" className="landing__button">
-							Shop now!
-						</Link>
-					</div>
+					<h2 className="landing__h1">Kick Culture</h2>
+					<p className="landing__p">
+						Walk in
+						<span style={{ color: "#F06D58" }}> style</span>, breathe in
+						<span style={{ color: "#FCED76" }}> culture</span>:
+						<br />
+						<span style={{ textShadow: "0 0 5px var(--violet)" }}>
+							Kick Culture
+						</span>{" "}
+						- in every pair, there&apos;s a history!
+					</p>
+					<Link to="/products" className="landing__button">
+						Shop now!
+					</Link>
 				</div>
-			</div>
+			</section>
+
+			<section>
+				{/* eslint-disable-next-line react/no-unknown-property */}
+				<marquee scrollamount="8" behavior="scroll" direction="right">
+					🤩 Discounts up to 25%! 🤩
+				</marquee>
+			</section>
 		</motion.div>
 	);
 };
